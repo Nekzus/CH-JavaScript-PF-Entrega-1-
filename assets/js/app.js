bd60d8f5ef7a4d1a55508d46f52b02123e2e662a
Nekzus <85507589+Nekzus@users.noreply.github.com>
2021-10-21 00:59:01 -0300

    Mejora Codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,14 @@
 //** los cuales se cargan como objetos dentro de un array que actua como carrito y se guarda en el localstorage  */
 
 //**** DECLARACION DE VARIABLES GLOBALES ****/
-
+let total= 0; //Variable para el monto total del carrito.
 let confirmacion0 = true; // Confirmacion de Borrado Item. Inicialización.
 let cantidad= 0; // Variable para contabilizar la cantidad del mismo item en el carrito .
 let codigo = 0; // Inicializo el identificador del item, el cual se incrementara con la creacion de cada objeto(futuro: proxima entrega).
 let codigoBorrar = 0; // Variable que almacena el numero de item a borrar ingresado por el usuario. Inicialización.
 let carritoProductos = JSON.parse(localStorage.getItem('carritoProductos')) || []; // Array que almacena los items ingresados por el usuario a modo de objetos. Se realiza lectura del array almacenado en localStorage.
 let printHtml = document.getElementById('printHtml');// Referencia variable al cuadro de productos ingresados en el DOM.
-let videoJuego; // Variable para insertar el codigo HTML en la tabla.
+let productos; // Variable para insertar el codigo HTML en la tabla.
 const btnDelet = document.querySelector("#btnDelet"); // Como parte de sumar optimizacion al codigo, aplique algo de interaccion con el codigo HTML mediante un boton de borrado, y bootstrap.
 const btnDeletAll = document.querySelector('#btnDeletAll'); // Como parte de sumar optimizacion al codigo, aplique algo de interaccion con el codigo HTML mediante un boton de borrar todo, y bootstrap.
 const btnCarro1 = document.querySelector('#btnCarro1'); // Como parte de sumar optimizacion al codigo, aplique algo de interaccion con el codigo HTML mediante un boton de modificacion de item, y bootstrap.
@@ -25,17 +25,20 @@ const cantidadItem = document.querySelector('#casillaCantidad'); // Como parte d
 const imprimirEnHtml = (item) => {
     printHtml.innerHTML = ""; // Se limpia cuerpo de tabla.
     for(item of item) {
-        videoJuego = document.createElement('tr');
-        videoJuego.innerHTML = `<th scope="row">${item.titulo}</th>
+        productos = document.createElement('tr');
+        productos.innerHTML = `<th scope="row">${item.titulo}</th>
                                 <td>${item.codigo}</td>
                                 <td>$${item.precio}</td>
                                 <td><input id='casillaCantidad' class='text-center' type='number' value='${cantidad}'></input></td>`;
                                 
-        printHtml.appendChild(videoJuego);
+        printHtml.appendChild(productos);
     }
 }
-
+//**** FUNCION DE FILTRADO TITULO ITEMS ****//
 const filtroPorTitulo = (titulo )=> carritoProductos.filter(producto => producto.titulo === titulo); //Filtro de titulo para contabilizar cuantas veces el producto se ingreso al carrito.
+
+//*** FUNCION DE BUSQUEDA POR CODIGO ITEM ****//
+const busquedaPorCodigo = (codigo) => carritoProductos.find(producto => producto.codigo === codigo); // Se verifica la existencia del item dentro del array videoJuegos almacenado.
 
 imprimirEnHtml(carritoProductos); // Se imprime en el cuerpo de la tabla HTML los datos guardados en el localStorage.
 
@@ -69,7 +72,6 @@ carritoProductos.push(item);
 localStorage.setItem('carritoProductos', JSON.stringify(carritoProductos)); // Se almacena en el localStorage el nuevo objeto-item creado.
 
 location.reload(); // Se refresca el navegador para que se muestren los cambios.
-imprimirEnHtml(carritoProductos);
 
 }
 
@@ -80,7 +82,7 @@ class Carrito {
         this.codigo = codigo;
         this.precio = precio;
         this.cantidad = cantidad;
-    }
+    }  
 }
 
 btnDelet.addEventListener("click", () => { // Llamado borrado de items mediante click del boton en el HTML.
@@ -88,15 +90,15 @@ btnDelet.addEventListener("click", () => { // Llamado borrado de items mediante 
     //**** MODO MODIFICACIÓN: BORRADO DE ITEMS SELECCIONADOS POR EL USUARIO ****/
     if(carritoProductos.length > 0) {
         do {
-            codigoBorrar = parseInt(prompt("Ingrese el número de item a borrar")); // Se solicita al usuario el numero de item a borrar.
+            codigoBorrar = parseInt(prompt("Ingrese el código del producto a borrar")); // Se solicita al usuario el numero de item a borrar.
             if (isNaN(codigoBorrar)) { // En caso de cancelacion del borrado, se sale del do... while.
                 confirmacion0 = false;
-            } else if (busquedaPorIdtm(codigoBorrar) !== undefined) { // Si se comprueba la existencia del item a borrar, se procede con el borrado. 
-                confirmacion0 = confirm(`Desea quitar item #${codigoBorrar} del listado?`); // Se confirma si en verdad se desea borrar el item.
-                carritoProductos = JSON.parse(localStorage.getItem('videoJuegos')); // Se lee el array almacenado en el localStorage.
-                let indexItemBorrar = carritoProductos.findIndex(videoJuego => videoJuego.codigo === codigoBorrar); // Se obtiene el numero de posicion en el array del item a borrar.
+            } else if (busquedaPorCodigo(codigoBorrar) !== undefined) { // Si se comprueba la existencia del item a borrar, se procede con el borrado. 
+                confirmacion0 = confirm(`Desea quitar el código #${codigoBorrar} del listado?`); // Se confirma si en verdad se desea borrar el item.
+                carritoProductos = JSON.parse(localStorage.getItem('carritoProductos')); // Se lee el array almacenado en el localStorage.
+                let indexItemBorrar = carritoProductos.findIndex(producto => producto.codigo === codigoBorrar); // Se obtiene el numero de posicion en el array del item a borrar.
                 carritoProductos.splice(indexItemBorrar, 1); // Se ejecuta el borrado.
-                localStorage.setItem('videoJuegos', JSON.stringify(carritoProductos)); // Se almacena el array con el item borrado.
+                localStorage.setItem('carritoProductos', JSON.stringify(carritoProductos)); // Se almacena el array con el item borrado.
                 location.reload(); 
 
             } else {
@@ -110,6 +112,7 @@ btnDelet.addEventListener("click", () => { // Llamado borrado de items mediante 
 btnDeletAll.addEventListener("click", () => { // Llamado borrado de todos los items mediante click del boton en el HTML.
     //**** MODO MODIFICACIÓN: BORRADO DE TODOS LOS ITEMS ****/
     if(carritoProductos.length >0) {
+            carritoProductos = [];
             localStorage.clear();
             location.reload();
         }
